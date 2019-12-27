067f2ab97ceddf54acac4b0d76f585e7e4e873d2
firemaples <firemaples@gmail.com>
2019-12-27 16:55:41 +0800

    Implement Getui SDK 2.12.5.0

--- app/src/main/java/com/firemaples/getuitest/CoreApplication.java
+++ app/src/main/java/com/firemaples/getuitest/CoreApplication.java
@@ -4,18 +4,12 @@ import android.app.Application;
 import com.igexin.sdk.PushManager;
 
 public class CoreApplication extends Application {
-    private Logger logger = LoggerFactory.getLogger(CoreApplication.class);
-
     @Override
     public void onCreate() {
         super.onCreate();
 
-        logger.debug("onCreate()");
-
         PushManager.getInstance().initialize(this, GetuiPushService.class);
         PushManager.getInstance().registerPushIntentService(this, GetuiMessageService.class);
-
-        PushManager.getInstance().setPrivacyPolicyStrategy(this, true);
 //        PushManager.getInstance().turnOnPush(this);
 //        PushManager.getInstance().bindAlias(this, "abcd");
 //        PushManager.getInstance().setSilentTime(this, 0, 23);
--- app/src/main/java/com/firemaples/getuitest/GetuiPushService.kt
+++ app/src/main/java/com/firemaples/getuitest/GetuiPushService.kt
@@ -1,34 +1,39 @@
 package com.firemaples.getuitest
 
+import android.app.Service
 import android.content.Intent
 import android.os.IBinder
-import com.igexin.sdk.PushService
+import com.igexin.sdk.GTServiceManager
 
-class GetuiPushService : PushService() {
+class GetuiPushService : Service() {
     private val logger: Logger = LoggerFactory.getLogger(GetuiPushService::class.java)
 
     override fun onBind(intent: Intent?): IBinder? {
         logger.info("onBind()")
-        return super.onBind(intent)
+        return GTServiceManager.getInstance().onBind(intent)
     }
 
     override fun onCreate() {
         super.onCreate()
         logger.info("onCreate()")
+        GTServiceManager.getInstance().onCreate(this)
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        super.onStartCommand(intent, flags, startId)
         logger.info("onStartCommand(), intent: ${Utils.intentToString(intent)}, flags: $flags, startId: $startId")
-        return super.onStartCommand(intent, flags, startId)
+        return GTServiceManager.getInstance().onStartCommand(this, intent, flags, startId)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         logger.info("onDestroy()")
+        GTServiceManager.getInstance().onDestroy()
     }
 
     override fun onLowMemory() {
         super.onLowMemory()
         logger.info("onLowMemory()")
+        GTServiceManager.getInstance().onLowMemory()
     }
 }
