7787873ec352702f96cf9e670434188985446cc6
firemaples <firemaples@gmail.com>
2019-12-27 16:59:05 +0800

    Update Getui SDK to com.getui:sdk-for-google-play:4.3.8.0

--- app/src/main/java/com/firemaples/getuitest/GetuiPushService.kt
+++ app/src/main/java/com/firemaples/getuitest/GetuiPushService.kt
@@ -1,39 +1,34 @@
 package com.firemaples.getuitest
 
-import android.app.Service
 import android.content.Intent
 import android.os.IBinder
-import com.igexin.sdk.GTServiceManager
+import com.igexin.sdk.PushService
 
-class GetuiPushService : Service() {
+class GetuiPushService : PushService() {
     private val logger: Logger = LoggerFactory.getLogger(GetuiPushService::class.java)
 
     override fun onBind(intent: Intent?): IBinder? {
         logger.info("onBind()")
-        return GTServiceManager.getInstance().onBind(intent)
+        return super.onBind(intent)
     }
 
     override fun onCreate() {
         super.onCreate()
         logger.info("onCreate()")
-        GTServiceManager.getInstance().onCreate(this)
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        super.onStartCommand(intent, flags, startId)
         logger.info("onStartCommand(), intent: ${Utils.intentToString(intent)}, flags: $flags, startId: $startId")
-        return GTServiceManager.getInstance().onStartCommand(this, intent, flags, startId)
+        return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         logger.info("onDestroy()")
-        GTServiceManager.getInstance().onDestroy()
     }
 
     override fun onLowMemory() {
         super.onLowMemory()
         logger.info("onLowMemory()")
-        GTServiceManager.getInstance().onLowMemory()
     }
 }
--- app/src/main/java/com/firemaples/getuitest/MainActivity.kt
+++ app/src/main/java/com/firemaples/getuitest/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.firemaples.getuitest
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
